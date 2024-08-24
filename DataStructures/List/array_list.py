f29807ff5cae7e80f844274a75ef13fd1f249edd
danielmatabajoy <d.matabajoy@uniandes.edu.co>
2024-08-24 10:26:26 -0500

    cambio array Est-2

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -1,9 +1,42 @@
-def new list():
+def new_list():
     lst={
-        "elements":{}
+        "elements":[]
         "size":0
-        
-    }
+        }
     return lst
+
 def add_first(lst, elem):
-    lst["elements"]
+    lst["elements"].insert(0,elem)
+    lst["size"]+=1
+    return lst
+
+def is_empty (lst):
+    return len(lst)==0
+
+def last_element(lst):
+    if not lst: 
+        return None
+    return lst[-1]
+ 
+
+def remove_last(lst):
+    if not lst:
+        return None
+    else:
+        last_element=lst.pop()
+        return last_element
+        
+def delete_element(lst,pos):
+    if 0 <= pos < len(lst):
+        lst.pop(pos) 
+    else:
+        return None
+    
+def sub_list(lst,pos,numelem):
+    if pos < len(lst) and numelem <= len(lst)-pos:
+        copia = lst[pos:pos + numelem] 
+        return copia
+    else:
+        raise Exception("Posición o número de elementos no válidos.")
+    
+    
