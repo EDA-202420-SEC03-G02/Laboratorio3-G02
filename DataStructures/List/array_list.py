271938b82c5a6ce7737c731daa33e7aa172bffa5
j.sanchezf@uniandes.edu.co <j.sanchezf@uniandes.edu.co>
2024-08-24 10:24:52 -0500

    Cambios Array Julian

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -1,9 +1,23 @@
-def new list():
-    lst={
-        "elements":{}
-        "size":0
-        
-    }
+
+  
+from DataStructures.List.Tests.test_array_list import test_is_empty
+
+
+def get_size(lst):
+    tamaño_lista=lst["size"]
+    return tamaño_lista
+def get_element(lst, pos):
+    return lst["elements"][pos]
+
+def insert_element(lst, elem, pos):
+    if is_empty==True:
+       lst.append(elem)
+       lst["Size"]+=1
+    else:
+       lst.insert(pos, elem)
+       lst["Size"]+=1
     return lst
-def add_first(lst, elem):
-    lst["elements"]
+
+def change_info(lst, pos, new_info):
+    lst["elements"][pos].append(new_info)
+    return lst 
