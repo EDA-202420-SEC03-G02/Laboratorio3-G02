33c32b80bce5fbc3b461f02278e4b6c1049e28e0
DavidAlvarez26 <df.alvarezr1@uniandes.edu.co>
2024-08-24 11:41:17 -0500

    cambios array list main

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -4,14 +4,36 @@ def new_list():
         "size":0
         }
     return lst
+
+def add_first(lst, elem):
+    lst["elements"].insert(0,elem)
+    lst["size"]+=1
+    return lst
+
 def add_last(lst, elem):
     lst["elements"].append(elem)  
     lst["size"] += 1
     return lst
-def get_first_element(lst):
-    
+
+def is_empty (lst):
+    return len(lst)==0
+
+def get_size(lst):
+    tamaño_lista=lst["size"]
+    return tamaño_lista
+
+def get_first_element(lst): 
+    if not lst: 
+        return None
     return lst["elements"][0]
-    
+
+def last_element(lst):
+    if not lst: 
+        return None
+    return lst[-1] 
+
+def get_element(lst, pos):
+    return lst["elements"][pos]  
     
 def remove_first(lst):
     if lst["size"] > 0:
@@ -19,7 +41,25 @@ def remove_first(lst):
         lst["size"] -= 1 
         return first_element
     else:
-        return None   
+        return None  
+    
+def remove_last(lst):
+    if lst["size"] == 0:
+        return None
+    else:
+        last_element = lst["elements"].pop()  
+        lst["size"] -= 1 
+        return last_element
+
+def insert_element(lst, elem, pos):
+    if lst["size"] == 0 or pos == lst["size"]:  
+        lst["elements"].append(elem)
+    else:
+        lst["elements"].insert(pos, elem) 
+    
+    lst["size"] += 1
+    return lst
+       
     
 def is_present(lst,elem, cmp_function):
     size=lst["size"]
@@ -32,40 +72,23 @@ def is_present(lst,elem, cmp_function):
                 break
         if keyexist:
             return keypos
-    return -1        
+    return -1 
+
+def delete_element(lst,pos):
+    if 0 <= pos < len(lst):
+        lst.pop(pos) 
+    else:
+        return None 
+    
+def change_info(lst, pos, new_info):
+    lst["elements"][pos] = new_info  
+    return lst          
                 
 def exchange(lst, pos1, pos2):   
     if pos1 >= 0 and pos2 >= 0 and pos1 < lst["size"] and pos2 < lst["size"]:
         lst["elements"][pos1], lst["elements"][pos2] = lst["elements"][pos2], lst["elements"][pos1]
         return lst     
-
-def add_first(lst, elem):
-    lst["elements"].insert(0,elem)
-    lst["size"]+=1
-    return lst
-
-def is_empty (lst):
-    return len(lst)==0
-
-def last_element(lst):
-    if not lst: 
-        return None
-    return lst[-1]
- 
-
-def remove_last(lst):
-    if not lst:
-        return None
-    else:
-        last_element=lst.pop()
-        return last_element
-        
-def delete_element(lst,pos):
-    if 0 <= pos < len(lst):
-        lst.pop(pos) 
-    else:
-        return None
-    
+  
 def sub_list(lst,pos,numelem):
     if pos < len(lst) and numelem <= len(lst)-pos:
         copia = lst[pos:pos + numelem] 
@@ -76,24 +99,11 @@ def sub_list(lst,pos,numelem):
     
 
   
-from DataStructures.List.Tests.test_array_list import test_is_empty
 
 
-def get_size(lst):
-    tamaño_lista=lst["size"]
-    return tamaño_lista
-def get_element(lst, pos):
-    return lst["elements"][pos]
 
-def insert_element(lst, elem, pos):
-    if is_empty==True:
-       lst.append(elem)
-       lst["Size"]+=1
-    else:
-       lst.insert(pos, elem)
-       lst["Size"]+=1
-    return lst
 
-def change_info(lst, pos, new_info):
-    lst["elements"][pos].append(new_info)
-    return lst 
+
+
+
+ 
